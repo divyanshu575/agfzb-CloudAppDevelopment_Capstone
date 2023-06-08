6bb724b7b180ccee85e383e7c12c96ae13b8050d
divyanshu575 <132145488+divyanshu575@users.noreply.github.com>
2023-06-08 14:42:55 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,108 +2,155 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, DealerReview, CarMake, CarModel
+from .restapis import get_dealer_by_id_from_cf, get_dealers_from_cf, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
 
-
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-def about(request):
-    context = {}
-    return render(request, 'djangoapp/about.html', context)
-
-
-def contact(request):
-    context = {}
-    return render(request, 'djangoapp/contact.html', context)
-
-
-def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
+def about(request):
+    return render(request, 'djangoapp/about.html')
 # ...
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
-
+def contact(request):
+    return render(request, 'djangoapp/contact.html') 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 def login_request(request):
     context = {}
     if request.method == "POST":
         username = request.POST['username']
-        password = request.POST['pwd']
+        password = request.POST['psw']
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'djangoapp/index.html', context)
+            return redirect('djangoapp:index')
         else:
             context['message'] = "Invalid username or password."
-            return render(request, 'djangoapp/index.html', context)
+            return render(request, 'djangoapp/user_login.html', context)
     else:
-        context['message'] = "Invalid request."
-        return render(request, 'djangoapp/index.html', context)
-# ...
+        return render(request, 'djangoapp/login.html', context)
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 def logout_request(request):
-    print("Log out the user '{}'".format(request.user.username))
+    # Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
     logout(request)
+        # Redirect user back to course list view
     return redirect('djangoapp:index')
 
-
 # Create a `registration_request` view to handle sign up request
+# def registration_request(request):
 def registration_request(request):
     context = {}
+    # If it is a GET request, just render the registration page
     if request.method == 'GET':
-        return render(request, 'djangoapp/registration.html', context)
+        return render(request, 'djangoapp/user_registration.html', context)
+    # If it is a POST request
     elif request.method == 'POST':
-        # Check if user exists
+        # Get user information from request.POST
         username = request.POST['username']
         password = request.POST['psw']
         first_name = request.POST['firstname']
         last_name = request.POST['lastname']
         user_exist = False
         try:
+            # Check if user already exists
             User.objects.get(username=username)
             user_exist = True
         except:
-            logger.error("New user")
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
         if not user_exist:
+            # Create user in auth_user table
             user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
                                             password=password)
-            user.is_superuser = True
-            user.is_staff=True
-            user.save()  
+            # Login the user and redirect to course list page
             login(request, user)
             return redirect("djangoapp:index")
         else:
-            messages.warning(request, "The user already exists.")
-            return redirect("djangoapp:registration")
+            return render(request, 'djangoapp/user_registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-
-
+def get_dealerships(request):
+    if request.method == "GET":
+        context = {}
+        url = "https://us-east.functions.appdomain.cloud/api/v1/web/3c9f873f-d7cc-496c-9831-4447aaba0e81/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        context["dealership_list"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, id):
+    if request.method == "GET":
+        context = {}
+        dealer_url = "https://us-east.functions.appdomain.cloud/api/v1/web/3c9f873f-d7cc-496c-9831-4447aaba0e81/api/dealership"
+        dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
+        context["dealer"] = dealer
+        review_url = "https://us-east.functions.appdomain.cloud/api/v1/web/3c9f873f-d7cc-496c-9831-4447aaba0e81/api/review"
+        reviews = get_dealer_reviews_from_cf(review_url, id=id)
+        #print(reviews)
+        context["reviews"] = reviews
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
+def add_review(request, id):
+    context = {}
+    dealer_url = "https://us-east.functions.appdomain.cloud/api/v1/web/3c9f873f-d7cc-496c-9831-4447aaba0e81/api/dealership"
+    dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
+    context["dealer"] = dealer
+    if request.method == 'GET':
+        # Get cars for the dealer
+        cars = CarModel.objects.filter(id=id)
+        print(cars)
+        context["cars"] = cars
+        
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            username = request.user.username
+            print(request.POST)
+            payload = dict()
+            car_id = request.POST["car"]
+            car = CarModel.objects.get(pk=car_id)
+            payload["time"] = datetime.utcnow().isoformat()
+            payload["name"] = username
+            payload["dealership"] = id
+            payload["id"] = id
+            payload["review"] = request.POST["content"]
+            payload["purchase"] = False
+            if "purchasecheck" in request.POST:
+                if request.POST["purchasecheck"] == 'on':
+                    payload["purchase"] = True
+            payload["purchase_date"] = request.POST["purchasedate"]
+            payload["car_make"] = car.make.name
+            payload["car_model"] = car.name
+            payload["car_year"] = int(car.year.strftime("%Y"))
+
+            new_payload = {}
+            new_payload["review"] = payload
+            review_post_url = "https://us-east.functions.appdomain.cloud/api/v1/web/3c9f873f-d7cc-496c-9831-4447aaba0e81/api/post-review"
+            post_request(review_post_url, new_payload, id=id)
+        return redirect("djangoapp:dealer_details", id=id)      
