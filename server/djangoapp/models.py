f6bb09f60a070d90d5a9aab1964e222c30725dde
divyanshu575 <132145488+divyanshu575@users.noreply.github.com>
2023-06-08 14:03:02 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30, default='car make')
+    description = models.CharField(max_length=1000, default='car make description')
 
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'SEDAN'
+    SUV = 'SUV'
+    TRUCK = 'TRUCK'
+    TYPE_CHOICES = (
+        (SEDAN, "SEDAN"),
+        (SUV, "SUV"),
+        (TRUCK, "TRUCK")
+    )
+    dealer_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=30, default='car model')
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES, default=SEDAN)
+    year = models.DateField(default=now)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "DealerId: " + self.dealer_id.__str__() + "," + \
+               "Type: " + self.type + "," + \
+               "Year: " + self.year.__str__()
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
